156a4f3e5af612bae1dd89df35d9442dd87e97ec
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-11-12 21:25:55 -0800

    Update main.c

--- lab11_scheduler/source/main.c
+++ lab11_scheduler/source/main.c
@@ -12,12 +12,63 @@
 #include "simAVRHeader.h"
 #endif
 
+unsigned char GetKeypadKey(){
+    PORTC = 0xEF;
+    asm("nop");
+    if (GetBit(PINC,0)==0) { return('1'); }
+    if (GetBit(PINC,1)==0) { return('4'); }
+    if (GetBit(PINC,2)==0) { return('7'); }
+    if (GetBit(PINC,3)==0) { return('*'); }
+    
+    PORTC = 0xDF;
+    asm("nop");
+    if (GetBit(PINC,0)==0) { return('2'); }
+    if (GetBit(PINC,1)==0) { return('5'); }
+    if (GetBit(PINC,2)==0) { return('8'); }
+    if (GetBit(PINC,3)==0) { return('0'); }
+    
+    PORTC = 0xBF;
+    asm("nop");
+    if (GetBit(PINC,0)==0) { return('3'); }
+    if (GetBit(PINC,1)==0) { return('6'); }
+    if (GetBit(PINC,2)==0) { return('9'); }
+    if (GetBit(PINC,3)==0) { return('#'); }
+    
+    PORTC = 0x7F;
+    asm("nop");
+    if (GetBit(PINC,0)==0) { return('A'); }
+    if (GetBit(PINC,1)==0) { return('B'); }
+    if (GetBit(PINC,2)==0) { return('C'); }
+    if (GetBit(PINC,3)==0) { return('D'); }
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    unsigned char x;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xF0; PORTC = 0x0F;
     /* Insert your solution below */
     while (1) {
-
+        x = GetKeypadKey();
+        switch (x) {
+            case '\0': PORTB = 0x1F; break;
+            case '1': PORTB = 0x01; break;
+            case '2': PORTB = 0x02; break;
+            case '3': PORTB = 0x03; break;
+            case '4': PORTB = 0x04; break;
+            case '5': PORTB = 0x05; break;
+            case '6': PORTB = 0x06; break;
+            case '7': PORTB = 0x07; break;
+            case '8': PORTB = 0x08; break;
+            case '9': PORTB = 0x09; break;
+            case 'A': PORTB = 0x0A; break;
+            case 'B': PORTB = 0x0B; break;
+            case 'C': PORTB = 0x0C; break;
+            case 'D': PORTB = 0x0D; break;
+            case '*': PORTB = 0x0E; break;
+            case '0': PORTB = 0x00; break;
+            case '#': PORTB = 0x0F; break;
+            default: PORTB = 0x1B; break;
     }
     return 1;
 }
