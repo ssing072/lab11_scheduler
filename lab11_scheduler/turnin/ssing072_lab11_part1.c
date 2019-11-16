a327e783f16e9acac107bacd978daae90f49d841
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-11-15 16:35:06 -0800

    Create ssing072_lab11_part1.c

---
+++ lab11_scheduler/turnin/ssing072_lab11_part1.c
@@ -0,0 +1,107 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "keypad.h"
+#include "timer.h"
+#include "io.h"
+
+
+
+
+typedef struct _task {
+	signed char state;
+	unsigned long int period;
+	unsigned long int elapsedTime;
+	int (*TickFct)(int);
+} task;
+
+unsigned long int findGCD(unsigned long int a, unsigned long int b)
+{
+	unsigned long int c;
+	while(1){
+		c = a % b;
+		if( c == 0 ) { return b; }
+		a = b;
+		b = c;
+	}
+	return 0;
+}
+
+enum keyPad_states{ key };
+unsigned char x;
+
+int keyPad(int state){
+	
+	switch(state){
+		case key:
+		state = key;
+		break;
+	}
+	switch(state){
+		case key:
+			x = GetKeypadKey();
+			switch (x) {
+				case '\0': PORTB = 0x1F; break;
+				case '1': PORTB = 0x01; break;
+				case '2': PORTB = 0x02; break;
+				case '3': PORTB = 0x03; break;
+				case '4': PORTB = 0x04; break;
+				case '5': PORTB = 0x05; break;
+				case '6': PORTB = 0x06; break;
+				case '7': PORTB = 0x07; break;
+				case '8': PORTB = 0x08; break;
+				case '9': PORTB = 0x09; break;
+				case 'A': PORTB = 0x0A; break;
+				case 'B': PORTB = 0x0B; break;
+				case 'C': PORTB = 0x0C; break;
+				case 'D': PORTB = 0x0D; break;
+				case '*': PORTB = 0x0E; break;
+				case '0': PORTB = 0x00; break;
+				case '#': PORTB = 0x0F; break;
+				default: PORTB = 0x1B; break;
+			}
+		break;
+	}
+	return state;
+}
+
+int main(void)
+{
+	DDRA = 0xFF; PORTA = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xF0; PORTC = 0x0F; 
+	DDRD = 0xFF; PORTD = 0x00;
+	
+	static task task1;
+	task *tasks[] = { &task1};
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+		
+	task1.state = key;
+	task1.period = 10;
+	task1.elapsedTime = task1.period;
+	task1.TickFct = &keyPad;
+	
+	 
+	unsigned short i;
+	unsigned long GCD = tasks[0]->period;
+	for(i=1;i<numTasks; i++){
+		GCD = findGCD(GCD, tasks[i]->period);
+	}
+	
+	TimerSet(GCD);
+	TimerOn();
+	
+	LCD_init();
+	
+    while (1) 
+    {
+		for (i = 0; i < numTasks; i++){
+			if(tasks[i]->elapsedTime == tasks[i]->period){
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				tasks[i]->elapsedTime = 0;	
+			}
+			tasks[i]->elapsedTime += GCD;//GCD
+		}
+		while(!TimerFlag){};
+		TimerFlag = 0;
+    }
+}
